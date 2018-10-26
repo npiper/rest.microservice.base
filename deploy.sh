47a4d849e0dc46afa556c7098d3ce0b2e412b8b3
npiper <solveapuzzledev@gmail.com>
2018-10-26 23:15:47 +0100

    Updated build scripts

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-git config --global user.name "${GIT_USER_NAME}"
-git config --global user.email "${GIT_USER_EMAIL}"
-mvn deploy scm:tag -Drevision=${TRAVIS_BUILD_NUMBER}.$(git rev-parse --short HEAD) -Dcobertura.skip -Dusername=${GIT_USER_NAME} -Dpassword=${GITPW}
